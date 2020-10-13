6e20801f0f27db34ecfed0d3a628563c4f342dd0
Timmy <timmycaris1904@gmail.com>
2020-10-13 13:11:45 +0530

    First commit

---
+++ javascriptfile.js
@@ -0,0 +1,32 @@
+function CheckPassword() 
+{     
+    var password = document.getElementById("pass").value;
+    if(password=="lolnetflix") 
+    { 
+        $('#verify').modal('hide');
+        $('#exampleModal').modal('show');
+    }
+    else
+    { 
+        alert('wrong');
+    }
+}
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+
